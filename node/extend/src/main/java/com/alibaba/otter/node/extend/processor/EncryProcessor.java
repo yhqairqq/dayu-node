2099dd3bd90c8f12d6759ad2afbf72a7e794f241
yanghuanqing@wdai.com <304187514@qq.com>
2019-05-23 14:41:27 +0800

    no message

---
+++ node/extend/src/main/java/com/alibaba/otter/node/extend/processor/EncryProcessor.java
@@ -0,0 +1,7 @@
+package com.alibaba.otter.node.extend.processor;
+
+/**
+ * Created by yanghuanqing@wdai.com on 2018/6/19.
+ */
+public class EncryProcessor {
+}
---
+++ shared/etl/src/main/java/com/alibaba/otter/shared/etl/extend/processor/support/FilterFetcherAware.java
@@ -0,0 +1,7 @@
+package com.alibaba.otter.shared.etl.extend.processor.support;
+
+/**
+ * Created by yanghuanqing@wdai.com on 2018/6/19.
+ */
+public interface FilterFetcherAware {
+}
